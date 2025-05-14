3423a9999cede1da73e0d7baa3911baee264f408
Bill Antoniadis <bill.antoniadis@streamline.us>
2025-05-14 15:13:34 -0400

    minor doc update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 MCP Gateway Contributors
+// Copyright (C) 2025 MCP Streamline Tech LLC
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
